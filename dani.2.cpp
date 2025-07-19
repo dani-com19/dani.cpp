34b50ed8511c7114278e4fb98b7164fe3f24ee8a
dani-com19 <danielmekuriaw33@gmail.com>
2025-07-19 03:25:22 -0700

    Add files via upload

---
+++ dani.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () { int a,b,sum ;
+    cout<<"hello "<<endl;
+	cout<<"enter a number "<<endl;
+	cin>>a;
+	cout<<"enter a scond number "<<endl;
+	cin>>b;
+	sum = a * b ;
+	cout<<"sum:"<<sum<<endl;
+	cout<<"sat,jul 19  and time :";
+	cout<<"1:09";
+	return 0;
+	
+}
