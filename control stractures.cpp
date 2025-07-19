529a2e5c16c397a0ec00f23f45d701a38ee141be
dani-com19 <danielmekuriaw33@gmail.com>
2025-07-19 04:03:40 -0700

    Add files via upload

---
+++ control stractures.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int  age = 10;
+	
+	cout<<"enter youer age:" ;
+	cin>>age;
+	if ( age > 7 ) {
+		cout<< "You can use a computer";
+	}
+	else {
+		cout<<"you can not use computer";
+	}
+}
