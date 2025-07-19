cc23e6becf1ce916ecf069c1811598a15258046d
dani-com19 <danielmekuriaw33@gmail.com>
2025-07-19 03:35:08 -0700

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int age= 10 ;
+	float height = 1.35 ;
+	string name = "daniel" ;
+	cout<<name<<"is"<<age<<" years old and " << height <<"mtall.";
+}
